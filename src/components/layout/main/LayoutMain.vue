a55b03078b3bc2cac1e3139d98049b15ddf3082f
cosmoscatts <2138889191@qq.com>
2023-03-27 13:39:41 +0800

    chore: update

--- src/components/layout/main/LayoutMain.vue
+++ src/components/layout/main/LayoutMain.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const uiStore = useUiStore()
 const tabStore = useTabStore()
 </script>
 
@@ -8,7 +9,7 @@ const tabStore = useTabStore()
     <RouterView v-slot="{ Component, route }">
       <CommonTransition>
         <KeepAlive :include="tabStore.cachedTabNames" :max="10">
-          <component :is="Component" :key="route.path" />
+          <component v-if="uiStore.reloadFlag" :is="Component" :key="route.path" />
         </KeepAlive>
       </CommonTransition>
     </RouterView>
