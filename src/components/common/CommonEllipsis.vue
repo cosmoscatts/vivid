32d1677449f28f643ed68e8a35a34fde0813c637
cosmoscatts <2138889191@qq.com>
2023-07-20 15:43:41 +0800

    feat: add component Ellipsis

--- src/components/common/CommonEllipsis.vue
+++ src/components/common/CommonEllipsis.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 defineOptions({ inheritAttrs: true })
 defineProps<{ content: string }>()
+
+const refEl = ref()
+const { isOutside } = useMouseInElement(refEl)
+
+const isEllipsis = $computed(() => {
+  if (!refEl.value)
+    return false
+  return refEl.value.scrollWidth > refEl.value.offsetWidth
+})
+
+const visible = computed(() => {
+  if (!isEllipsis)
+    return false
+  return !isOutside.value
+})
 </script>
 
 <template>
-  <span of-hidden text-ellipsis whitespace-nowrap>
-    <slot>
+  <a-tooltip :content="content" :popup-visible="visible">
+    <span ref="refEl" of-hidden text-ellipsis whitespace-nowrap>
       {{ content }}
-    </slot>
-  </span>
+    </span>
+  </a-tooltip>
 </template>
