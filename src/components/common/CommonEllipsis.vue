0cca30b6ce44e2cea29175be660d49ca0f05ca52
cosmoscatts <2138889191@qq.com>
2023-08-04 13:59:01 +0800

    chore: update

--- src/components/common/CommonEllipsis.vue
+++ src/components/common/CommonEllipsis.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
+import type { StyleValue } from 'vue'
+
 defineOptions({ inheritAttrs: true })
-defineProps<{ content: string }>()
+const { content, width = 100, tooltipPosition = 'lt', style = {} } = defineProps<{
+  content: string
+  width?: number
+  tooltipPosition?: 'top' | 'tl' | 'tr' | 'bottom' | 'bl' | 'br' | 'left' | 'lt' | 'lb' | 'right' | 'rt' | 'rb'
+  style?: StyleValue
+}>()
 
 const refEl = ref()
 const { isOutside } = useMouseInElement(refEl)
 
-const isEllipsis = $computed(() => {
-  if (!refEl.value) return false
-  return refEl.value.scrollWidth > refEl.value.offsetWidth
+let scrollWidth = $ref(0)
+let offsetWidth = $ref(0)
+
+function setWidth() {
+  if (!refEl.value) return
+  scrollWidth = refEl.value.scrollWidth
+  offsetWidth = refEl.value.offsetWidth
+}
+
+watch(() => content, (n, o) => {
+  if (n !== o) setWidth()
 })
+
+onMounted(setWidth)
+
+const isEllipsis = $computed(() => scrollWidth > offsetWidth)
 
 const visible = computed(() => {
   if (!isEllipsis) return false
@@ -17,9 +36,9 @@ const visible = computed(() => {
 </script>
 
 <template>
-  <a-tooltip :content="content" :popup-visible="visible">
-    <span ref="refEl" of-hidden text-ellipsis whitespace-nowrap>
+  <a-tooltip :content="content" :popup-visible="visible" :position="tooltipPosition">
+    <div ref="refEl" of-hidden text-ellipsis whitespace-nowrap :style="Object.assign(style, { maxWidth: `${width}px` })">
       {{ content }}
-    </span>
+    </div>
   </a-tooltip>
 </template>
