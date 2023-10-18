613b2aa9c9ac66cd05a9345f19c778feaa995fd6
cosmoscatts <2138889191@qq.com>
2023-10-18 16:00:52 +0800

    chore: update

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,21 +10,21 @@ import {
 export default defineConfig({
   shortcuts: [
     {
-    'hw-full': 'h-full w-full',
-    'hw-screen': 'h-screen w-screen',
-    'flex-center': 'flex justify-center items-center',
-    'flex-x-center': 'flex justify-center',
-    'flex-y-center': 'flex items-center',
-    'flex-col-center': 'flex-col flex-center',
-    'border-base': 'border-[rgb(var(--gray-3))]',
-    'icon-btn': 'op30 hover:op100 hover:bg-primary_hover',
-    'bg-body': 'bg-gray:15 dark:bg-[#17171A]',
-    'bg-nav': 'bg-white dark:bg-[#232324]',
-    'bg-side': 'bg-white dark:bg-[#232324]',
+      'hw-full': 'h-full w-full',
+      'hw-screen': 'h-screen w-screen',
+      'flex-center': 'flex justify-center items-center',
+      'flex-x-center': 'flex justify-center',
+      'flex-y-center': 'flex items-center',
+      'flex-col-center': 'flex-col flex-center',
+      'border-base': 'border-[rgb(var(--gray-3))]',
+      'icon-btn': 'op30 hover:op100 hover:bg-primary_hover',
+      'bg-body': 'bg-gray:15 dark:bg-[#17171A]',
+      'bg-nav': 'bg-white dark:bg-[#232324]',
+      'bg-side': 'bg-white dark:bg-[#232324]',
     },
     [/^btn-(\w+)$/, ([_, color]) => `op90 px2.5 py1 transition-all duration-200 ease-out no-underline! cursor-pointer hover:(text-${color} bg-${color}/10) border border-base! rounded`],
     [/^tag-(\w+)$/, ([_, color]) => `op90 px2.5 py1 transition-all duration-200 ease-out no-underline! cursor-pointer text-${color} bg-${color}/10 border border-base! rounded`],
-  ],  
+  ],
   rules: [
     [/^slide-enter-(\d+)$/, ([_, n]) => ({
       '--enter-stage': n,
